b5aec077e4dd5178114de2c3b56abc120999d987
Kristina-K96 <71282873+Kristina-K96@users.noreply.github.com>
2025-01-10 15:11:34 -0500

    updates to assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -32,13 +32,13 @@ mkdir ./data/processed/server_logs
 mkdir ./data/processed/user_logs 
 mkdir ./data/processed/event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/server*.log ./data/processed/server_logs
+cp ./data/raw/*server*.log ./data/processed/server_logs
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/user_log*.log ./data/processed/user_logs
-cp ./data/raw/event_log*.log ./data/processed/event_logs
+cp ./data/raw/*user*.log ./data/processed/user_logs
+cp ./data/raw/*event*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm -r ./data/raw/*ipaddr*
-rm -r ./data/processed/user_logs/*ipaddr* #There were no matches since I didn't copy the "ipaddr" files in step #6
+rm -r ./data/processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 ls ./data/processed/server_logs >> ./data/inventory.txt
 ls ./data/processed/user_logs >> ./data/inventory.txt
